a4d0dc492672f7200ca08091c01b4ec7ddc6cde0
ivan <menja08@yahoo.com>
2019-07-04 01:35:51 +0200

    initial commit, adds hailstone sequence code

---
+++ july2019/src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hailStone(14);
+	}
+	
+	public static void hailStone(int number) {
+		while(number > 1) {
+			if(number % 2 == 0) {
+				number = number / 2;
+			} else {
+				number = 3*number + 1;
+			}
+			System.out.println("Hailstone sequence = " + number);
+			
+		}
+		//return 0;
+		
+	}
+
+}
